0a13740ebd7f4df4d5ecb01cc99f52483285ee00
gierr7976 <gierr7976@gmail.com>
2021-11-14 22:01:15 +0300

    feature: Added context saving feature to Tap

--- lib/src/piping/tap.dart
+++ lib/src/piping/tap.dart
@@ -1,13 +1,19 @@
 part of piping;
 
 mixin Tap {
+  PipeContext? _context;
+
+  PipeContext get context => _context!;
+
   @protected
-  void dripper([PipeContext? nearestUpstreamContext]) => drip(
-        PipeContext(
-          currentTap: this,
-          nearestUpstreamContext: nearestUpstreamContext,
-        ),
-      );
+  void dripper([PipeContext? nearestUpstreamContext]) {
+    _context = PipeContext(
+      currentTap: this,
+      nearestUpstreamContext: nearestUpstreamContext,
+    );
+
+    drip(_context!);
+  }
 
   void drip(PipeContext context);
 }
