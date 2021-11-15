f29750895000951a3056696cccbde355d1462574
gierr7976 <gierr7976@gmail.com>
2021-11-15 11:16:40 +0300

    feature: added flutter_bloc export

--- lib/alchemica.dart
+++ lib/alchemica.dart
@@ -1,5 +1,7 @@
 library alchemica;
 
+export 'package:flutter_bloc/flutter_bloc.dart';
+
 export 'src/consumers/consumers.dart';
 export 'src/explosions/explosions.dart';
 export 'src/flasks/flasks.dart';
