14097651cc045998415fa5c577ae94a23c510500
gierr7976 <gierr7976@gmail.com>
2021-11-21 23:42:02 +0300

    docs: Added full documentation to PipeContext class

--- lib/src/piping/context.dart
+++ lib/src/piping/context.dart
@@ -1,15 +1,24 @@
 part of piping;
 
+/// Class contains useful information on [Tap]'s upstream surrounding.
 class PipeContext {
   final PipeContext? _nearestUpstreamContext;
   final Tap _currentTap;
 
+  /// Default constructor.
+  ///
+  /// Does not meant to be used outside [piping] library.
   const PipeContext({
     required Tap currentTap,
     PipeContext? nearestUpstreamContext,
   })  : _nearestUpstreamContext = nearestUpstreamContext,
         _currentTap = currentTap;
 
+  /// Looks up for [Tap] type of [U] in piping tree.
+  ///
+  /// Returns nearest instance of [U] if contained in piping.
+  ///
+  /// Throws a [LookupError] in other cases.
   U lookup<U>() {
     if (_currentTap is U) return _currentTap as U;
 
@@ -19,7 +28,11 @@ class PipeContext {
   }
 }
 
+/// Error class for lookup fails.
 class LookupError extends ArgumentError {
+  /// Default constructor.
+  ///
+  /// [lookedUp] — type being looked up.
   LookupError(Type lookedUp)
-      : super('There is no accessible $lookedUp in pipe system');
+      : super('There is no accessible $lookedUp in piping system');
 }
