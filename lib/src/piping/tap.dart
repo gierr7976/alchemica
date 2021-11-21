e929c66dc4a1de6c6baa212f2e5d385c471f11f6
gierr7976 <gierr7976@gmail.com>
2021-11-21 23:28:33 +0300

    docs: Added full documentation to Tap class

--- lib/src/piping/tap.dart
+++ lib/src/piping/tap.dart
@@ -1,10 +1,24 @@
 part of piping;
 
+/// Base piping class allowing to handle upstream updates.
 mixin Tap {
   PipeContext? _context;
 
+  /// Last dripped context.
+  ///
+  /// <br>
+  /// _Throws an error when accessed before first drip._
   PipeContext get context => _context!;
 
+  /// Performs actions needed to notify current [Tap] instance that there are
+  /// some upstream updates.
+  ///
+  /// <br>
+  /// [nearestUpstreamContext] — [PipeContext] of upstream [Tap] instance that
+  /// called this method.
+  ///
+  /// [Tap] instance should call this method on its children whenever
+  /// it updates.
   @protected
   void dripper([PipeContext? nearestUpstreamContext]) {
     _context = PipeContext(
@@ -15,5 +29,6 @@ mixin Tap {
     drip(_context!);
   }
 
+  /// Performs actions to handle upstream updates.
   void drip(PipeContext context);
 }
