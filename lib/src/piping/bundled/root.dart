2a21b030c1ff99664c54184cb1d6737a63f59bbb
gierr7976 <gierr7976@gmail.com>
2021-12-27 23:52:30 +0300

    docs: Added RootPipe reference

--- lib/src/piping/bundled/root.dart
+++ lib/src/piping/bundled/root.dart
@@ -1,18 +1,29 @@
 part of alchemica.piping;
 
+/// Root element of logic tree.
+///
+/// Intended to be used as logic tree swapper.
+/// Persists in the tree as shadowed root.
 class RootPipe implements Pipe {
   @override
   Label get label =>
       throw UnimplementedError('Root pipe should not have labels');
 
+  /// True root of current tree.
   Pipe? _child;
 
+  /// Matches downstream elements with given condition.
+  ///
+  /// Returns first matching element. If no matches, [null] is returned.
   @override
   T? find<T extends Pipe>([Label? label]) => _child?.find(label);
 
+  /// Extracts downstream element to preserve in
+  /// tree mutation.
   @override
   Map<Label, Pipe> extract() => _child!.extract();
 
+  /// Performs root swap with element preservation.
   void updateChild(Pipe newChild) {
     Map<Label, Pipe>? oldTreeMap = _child?.extract();
 
