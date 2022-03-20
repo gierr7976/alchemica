72c548d19af96d29c97a2d8d87cbfbee90d01c86
gierr7976 <gierr7976@gmail.com>
2022-03-20 22:29:23 +0300

    feat: Added export file

---
+++ lib/alchemica.dart
@@ -0,0 +1,8 @@
+library alchemica;
+
+export 'src/bundled/lib.dart';
+export 'src/common/lib.dart';
+export 'src/flasks/lib.dart';
+export 'src/installation/lib.dart';
+export 'src/piping/lib.dart';
+export 'src/preparations/lib.dart';
