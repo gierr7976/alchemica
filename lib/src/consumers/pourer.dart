6244e3dcc7b927e31ca536dbfc7a7a3898ea91ed
gierr7976 <gierr7976@gmail.com>
2022-03-23 17:52:50 +0300

    ref: Reworked Pourer as abstraction

--- lib/src/consumers/pourer.dart
+++ lib/src/consumers/pourer.dart
@@ -1,10 +1,9 @@
 part of alchemica.consumers;
 
-class Pourer<F extends Flask> extends StatelessWidget {
+abstract class Pourer<F extends Flask> extends StatelessWidget {
   static Widget defaultFallback(BuildContext context) => SizedBox();
 
   final Label? label;
-  final List<VialFactory> factories;
   final WidgetBuilder fallback;
   final bool strict;
 
@@ -18,7 +17,6 @@ class Pourer<F extends Flask> extends StatelessWidget {
   const Pourer({
     Key? key,
     this.label,
-    required this.factories,
     this.fallback = defaultFallback,
     this.strict = true,
   }) : super(key: key);
@@ -30,21 +28,11 @@ class Pourer<F extends Flask> extends StatelessWidget {
     if (flask is F)
       return BlocBuilder<FlaskBloc, Potion>(
         bloc: getFlask(context)?.bloc,
-        builder: buildWithAppropriate,
+        builder: forPotion,
       );
 
     return fallback(context);
   }
 
-  Vial buildWithAppropriate(BuildContext context, Potion potion) {
-    for (VialFactory factory in factories)
-      if (factory.checkPotion(potion)) return factory(potion);
-
-    throw StateError(
-      '''
-      Unable to find appropriate VialFactory for $potion!\n
-      Maybe you forgot to add this one?
-      ''',
-    );
-  }
+  Vial forPotion(BuildContext context, Potion potion);
 }
