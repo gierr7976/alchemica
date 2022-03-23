7fd2434e6d1ad492d6bc99da853de5d40377059a
gierr7976 <gierr7976@gmail.com>
2022-03-23 17:52:20 +0300

    feat: Updated export file

--- lib/alchemica.dart
+++ lib/alchemica.dart
@@ -1,8 +1,11 @@
 library alchemica;
 
 export 'src/bundled/lib.dart';
-export 'src/common/lib.dart';
 export 'src/flasks/lib.dart';
 export 'src/installation/lib.dart';
 export 'src/piping/lib.dart';
 export 'src/preparations/lib.dart';
+export 'src/fusing/lib.dart';
+export 'src/consumers/lib.dart';
+
+export 'package:flutter_bloc/flutter_bloc.dart';
