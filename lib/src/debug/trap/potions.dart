ad7764f9f2c24af573cb832b87e1d0e8ac7f04ca
gierr7976 <gierr7976@gmail.com>
2022-04-10 16:15:31 +0300

    ref: Provided toString to BrewedTrap

--- lib/src/debug/trap/potions.dart
+++ lib/src/debug/trap/potions.dart
@@ -13,4 +13,16 @@ class BrewedTrap extends BrewedPotion {
       BrewedTrap(
         caught: caught ?? this.caught,
       );
+
+  @override
+  String toString() {
+    const String title = 'BrewedTrap [\n';
+    const String end = ']';
+
+    final List<String> ingredients = [];
+    for (int i = 0; i < caught.length; i++)
+      ingredients.add('\t$i: ${caught[i].toString()}\n');
+
+    return title + ingredients.join() + end;
+  }
 }
