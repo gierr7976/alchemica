8146715ed6b848f69403b71e8d5d83fa3d44391c
gierr7976 <gierr7976@gmail.com>
2022-04-10 17:03:27 +0300

    ref: Reworked preparations

--- lib/src/preparations/ingredients.dart
+++ lib/src/preparations/ingredients.dart
@@ -5,6 +5,9 @@ abstract class Ingredient implements Prototype {
 
   @override
   Ingredient copyWith();
+
+  @override
+  String toString() => runtimeType.toString();
 }
 
 abstract class AlchemistIngredient extends Ingredient {
@@ -19,4 +22,31 @@ abstract class PipeIngredient extends Ingredient {
 
   @override
   PipeIngredient copyWith();
+}
+
+class DrippedIngredient extends Ingredient {
+  final Pipe _dripper;
+  final Potion _potion;
+
+  const DrippedIngredient({
+    required Pipe dripper,
+    required Potion potion,
+  })  : _dripper = dripper,
+        _potion = potion;
+
+  P extract<P extends Potion>() {
+    if (_potion is! P) throw ArgumentError('Incompatible potion!');
+
+    return _potion as P;
+  }
+
+  @override
+  DrippedIngredient copyWith({
+    Pipe? dripper,
+    Potion? potion,
+  }) =>
+      DrippedIngredient(
+        dripper: dripper ?? _dripper,
+        potion: potion ?? _potion,
+      );
 }
