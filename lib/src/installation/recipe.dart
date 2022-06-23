c3d9493215e6a34f1f4d755c7034100d4e0a3b43
gierr7976 <gierr7976@gmail.com>
2022-06-23 22:13:23 +0300

    fix: Recipe comparison

--- lib/src/installation/recipe.dart
+++ lib/src/installation/recipe.dart
@@ -4,4 +4,12 @@ abstract class Recipe {
   List<AlchemistIngredient> get whenBuilt => [];
 
   Pipe build();
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Recipe && runtimeType == other.runtimeType;
+
+  @override
+  int get hashCode => runtimeType.hashCode;
 }
